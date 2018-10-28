b261402e8e1c5477ca357d90d4219a929f85a67c
Tarma <tarmakatz@gmail.com>
2018-10-28 13:28:07 +0100

    Completed css grid layout template

---
+++ client/src/components/Grids/Grids.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './_grids.scss';
+
+const grids = () => {
+  return (
+    <section className="grids">
+      <div className="row">
+        <div className="col-1-of-2">Column 1 of 2</div>
+        <div className="col-1-of-2">Column 1 of 2</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-3">Column 1 of 3</div>
+        <div className="col-1-of-3">Column 1 of 3</div>
+        <div className="col-1-of-3">Column 1 of 3</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-3">Column 1 of 3</div>
+        <div className="col-2-of-3">Column 2 of 3</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-2-of-4">Column 2 of 4</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-3-of-4">Column 3 of 4</div>
+      </div>
+    </section>
+  );
+};
+
+export default grids;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 import Header from './Header/Header';
+import Grids from './Grids/Grids';
 
-export { Header };
+export { Header, Grids };
